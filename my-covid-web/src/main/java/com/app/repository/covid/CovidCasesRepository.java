014f3519ea9602b7736c28265d0ec2ce463ff527
KahouTong <elric.vlcy@gmail.com>
2021-03-19 14:38:30 +0800

    clean up practical

--- my-covid-web/src/main/java/com/app/repository/covid/CovidCasesRepository.java
+++ my-covid-web/src/main/java/com/app/repository/covid/CovidCasesRepository.java
@@ -10,12 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.app.entity.CovidCasesAreaEntity;
 
 public interface CovidCasesRepository extends JpaRepository<CovidCasesAreaEntity, UUID>  {
-
-//	@Query(value = "SELECT DISTINCT c.date, c.cases, c.id, c.fk_area_id FROM trx_covid_cases AS c order by date desc LIMIT 2", nativeQuery = true)
-//	List<CovidCasesAreaEntity> listLast2Records();
-
-//	@Query(value = "SELECT DISTINCT c.date, c.cases, c.id, c.fk_area_id FROM trx_covid_cases AS c order by date desc LIMIT 5", nativeQuery = true)
-//	List<CovidCasesAreaEntity> listLast5Records();
 	
 	// TODO: Practical bonus
 	// Change the Query above to Spring JPA Query or JPQL
